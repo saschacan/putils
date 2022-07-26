aa2f70c17aebcd87c3e1d86364fe5471356596a0
Sascha Can <Sascha.Can@pm.me>
2022-07-26 04:06:41 +0200

    move callbacks from init to connect function

--- putils/websocket.py
+++ putils/websocket.py
@@ -4,81 +4,80 @@ import websockets
 import logging
 import json
 
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator, Awaitable, Callable, Coroutine
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
 
-class WSClient():
+
+class WSClient:
     def __init__(self, url, **kwargs) -> None:
         self.url = url
+        self.timeout = kwargs.get("timeout") or 20
+        self.sleep = kwargs.get("sleep") or 2
 
-        self.timeout = kwargs.get('timeout') or 20
-        self.sleep = kwargs.get('sleep') or 2
+        self._reconnect = False
 
-        self.on_message = kwargs.get('on_message')
-        self.on_connect = kwargs.get('on_connect')
-        self.on_disconnect = kwargs.get('on_disconnect')
-        self.on_reconnect = kwargs.get('on_reconnect')
-        self.ping = kwargs.get('ping')
-    
     async def send(self, message) -> None:
         try:
             if self._connection.open:
                 try:
                     await self._connection.send(json.dumps(message))
                 except Exception as e:
-                    logging.error(f'Failed to send message. Reason: {e}')
+                    logging.error(f"Failed to send message. Reason: {e}")
             else:
-                logging.error('Connection is closed')
+                logging.error("Connection is closed")
         except AttributeError:
-            logging.error('No connection')
+            logging.error("No connection")
 
-    async def connect_forever(self) -> AsyncGenerator:
+    async def connect_forever(
+        self,
+        on_message: Callable[[Any], Any],
+        on_reconnect: Callable[[], Awaitable[None]],
+        ping: Callable[[], Coroutine[Any, Any, None]],
+    ) -> AsyncGenerator:
         while True:
             try:
                 async with websockets.connect(self.url) as ws:
                     self._connection = ws
-                    logging.info('Connected to websocket')
+                    logging.info("Connected to websocket")
 
-                    if self.ping:
-                        self._ping_task = asyncio.create_task(self.ping())
+                    if self._reconnect:
+                        self._reconnect = False
+                        await on_reconnect()
+                    self._ping_task = asyncio.create_task(ping())
 
                     while True:
-                        message = await asyncio.wait_for(ws.recv(), timeout=self.timeout)
+                        message = await asyncio.wait_for(
+                            ws.recv(), timeout=self.timeout
+                        )
 
                         try:
                             message = json.loads(message)
-                            if self.on_message:
-                                yield self.on_message(message)
-                            else:
-                                yield message
+                            yield on_message(message)
                         except json.JSONDecodeError:
-                            logging.debug(f'Not valid JSON - Message: {message}')
+                            logging.debug(f"Not valid JSON - Message: {message}")
             except asyncio.TimeoutError:
-                logging.error('Timeout Error')
-                await self._reconnect()
+                logging.error("Timeout Error")
+                self._reconnect = True
             except ConnectionRefusedError:
-                logging.error('Nobody seems to listen to this endpoint. Please check the URL.')
-                await self._reconnect()
+                logging.error(
+                    "Nobody seems to listen to this endpoint. Please check the URL."
+                )
+                self._reconnect = True
             except socket.gaierror:
-                logging.error('Socket Error')
-                await self._reconnect()
+                logging.error("Socket Error")
+                self._reconnect = True
             except ConnectionClosedOK:
-                logging.error('Connection was closed by endpoint')
-                await self._reconnect()
+                logging.error("Connection was closed by endpoint")
+                self._reconnect = True
             except ConnectionClosedError:
-                logging.error('Connection was closed because of an error')
-                await self._reconnect()
+                logging.error("Connection was closed because of an error")
+                self._reconnect = True
             except asyncio.exceptions.CancelledError:
-                logging.info('Disconnected from websocket')
-                if self.on_disconnect:
-                    self.on_disconnect()
+                logging.info("Disconnected from websocket")
                 return
             finally:
-                if self.ping:
-                    self._ping_task.cancel()
-    
-    async def _reconnect(self) -> None:
-        logging.info(f'Retrying connection in {self.sleep} seconds')
-        await asyncio.sleep(self.sleep)
-        if self.on_reconnect:
-            self.on_reconnect()
+                self._ping_task.cancel()
+
+                if self._reconnect:
+                    logging.info(f"Retrying connection in {self.sleep} seconds")
+                    await asyncio.sleep(self.sleep)
